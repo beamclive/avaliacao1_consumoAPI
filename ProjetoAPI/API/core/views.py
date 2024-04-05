82f2924f8ed37d6844d63e89cf2bd1497e31416a
Beatriz Machado de Oliveira <bmachado@eslsistemas.com.br>
2024-04-04 21:08:30 -0300

    Ajustes e inclusão da exibição dos dados do json

--- ProjetoAPI/API/core/views.py
+++ ProjetoAPI/API/core/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import HttpResponse
 import requests
 
-def MusicasArtistas(request): 
-    url = "https://api.deezer.com/chart"
-    resposta = requests.get(url)
+def MusicasArtistas(): 
+    try:
+        url = "https://api.deezer.com/chart/tracks"
+        resposta = requests.get(url)
 
-    if resposta.status_code == 200: 
-        return HttpResponse(resposta.content, content_type="application/json")
-    else: 
-        return HttpResponse("Erro ao acessar a API", status=resposta.status_code)
+        if resposta.status_code == 200: 
+            dados = resposta.json()
+
+            for track in dados['tracks']['data']:
+                # posicao no charts 
+                music_position = track['position']
+
+                # nome da musica
+                music_title = track['title_short']
+
+                # nome do artista
+                artist_name = track['artist']['name']
+
+                # nome do album
+                album_title = track['album']['title']
+
+                print(f"Posição: {music_position}")
+                print(f"Nome da música: {music_title}")
+                print(f"Artista: {artist_name}")
+                print(f"Álbum: {album_title}")
+                print()
+        else: 
+            return HttpResponse("Erro ao acessar a API", status=resposta.status_code)
+    except: return HttpResponse("Erro ao se conectar")
+
+MusicasArtistas()
